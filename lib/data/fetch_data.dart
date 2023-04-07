8e6cfd3af239b20e83ca9fa00df44d65113c7430
Kanelle Lorent <kanelle.lorent@mail-ecv.fr>
2023-04-07 15:21:38 +0200

    add favorite feature and page

--- lib/data/fetch_data.dart
+++ lib/data/fetch_data.dart
@@ -4,14 +4,14 @@ import 'dart:convert';
 import 'api_data.dart';
 import 'package:http/http.dart' as http;
 
-Future<List<Data>> fetchData() async {
+Future<List<DataStrains>> fetchData() async {
   var url = Uri.parse('https://weed-strain1.p.rapidapi.com/');
   final response = await http.get(url, headers: {
     "X-RapidAPI-Key": '24024d3c37msh77d432d3b0b04dbp1119dejsn66f40f00e220',
   });
   if (response.statusCode == 200) {
     List jsonResponse = json.decode(response.body);
-    return jsonResponse.map((data) => Data.fromJson(data)).toList();
+    return jsonResponse.map((data) => DataStrains.fromJson(data)).toList();
   } else {
     throw Exception('Unexpected error occured!');
   }
