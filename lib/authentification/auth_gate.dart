db94c05e0f7e65e65311e7bb13aa16d8469d3837
Kanelle Lorent <kanelle.lorent@mail-ecv.fr>
2023-04-25 12:27:23 +0200

    change some style for auth screen and add google sign-in

--- lib/authentification/auth_gate.dart
+++ lib/authentification/auth_gate.dart
@@ -3,6 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
 
 import '../main.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+Future<UserCredential> signInWithGoogle() async {
+  final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+  final GoogleSignInAuthentication googleAuth =
+      await googleUser!.authentication;
+
+  final OAuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth.accessToken,
+    idToken: googleAuth.idToken,
+  );
+
+  return await _auth.signInWithCredential(credential);
+}
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
@@ -13,10 +30,47 @@ class AuthGate extends StatelessWidget {
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
-          return const SignInScreen(
-            providerConfigs: [
+          return SignInScreen(
+            headerBuilder: (context, constraints, _) {
+              return Padding(
+                padding: EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image.asset('assets/images/Marijane.png'),
+                ),
+              );
+            },
+            footerBuilder: (context, constraints) {
+              return Column(children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      final UserCredential userCredential =
+                          await signInWithGoogle();
+                      // Handle successful sign-in
+                    } catch (e) {
+                      // Handle sign-in errors
+                    }
+                  },
+                  child: const Text('Sign in with Google'),
+                ),
+              ]);
+            },
+            providerConfigs: const [
               EmailProviderConfiguration(),
             ],
+            /* ElevatedButton(
+              onPressed: () async {
+                try {
+                  final UserCredential userCredential =
+                      await signInWithGoogle();
+                  // Handle successful sign-in
+                } catch (e) {
+                  // Handle sign-in errors
+                }
+              },
+              child: const Text('Sign in with Google'),
+            ), */
           );
         }
 
